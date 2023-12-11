bf528d22a51b2ce7c4765bb49bdff6965b5c87bd
Steak Barbare <cortodufour@gmail.com>
2023-12-12 00:19:03 +0100

    Add: Simple Components Example

--- src/Table.js
+++ src/Table.js
@@ -1,29 +1,41 @@
 import React, { Component } from "react";
 
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Type</th>
+      </tr>
+    </thead>
+  );
+};
+
+const TableBody = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>Chipolata</td>
+        <td>Classique</td>
+      </tr>
+      <tr>
+        <td>Merguez</td>
+        <td>Kipik</td>
+      </tr>
+      <tr>
+        <td>Knacki</td>
+        <td>À chier</td>
+      </tr>
+    </tbody>
+  );
+};
+
 class Table extends Component {
   render() {
     return (
       <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Chipolata</td>
-            <td>Classique</td>
-          </tr>
-          <tr>
-            <td>Merguez</td>
-            <td>Kipik</td>
-          </tr>
-          <tr>
-            <td>Knacki</td>
-            <td>À chier</td>
-          </tr>
-        </tbody>
+        <TableHeader />
+        <TableBody />
       </table>
     );
   }
