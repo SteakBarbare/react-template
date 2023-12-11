f0046b1809b8f612d637c05851210254183aa14c
Steak Barbare <cortodufour@gmail.com>
2023-12-12 00:16:52 +0100

    Add: Course Intro

---
+++ src/Test.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Table from "./Table";
+
+class Test extends Component {
+  render() {
+    return (
+      <div className="Test">
+        <h1>Saucisse</h1>
+        <Table />
+      </div>
+    );
+  }
+}
+
+export default Test;
